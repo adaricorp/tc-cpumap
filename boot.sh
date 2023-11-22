4325b364ce94b7652beb858bebfe9d1c1390e0ac
Brad Cowie <brad@faucet.nz>
2023-11-22 14:57:05 +1300

    ci: Add github action test and release workflows.
    Move build system to goreleaser.

---
+++ boot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euo pipefail
+
+/usr/sbin/bpftool btf dump file /sys/kernel/btf/vmlinux format c > bpf/common/vmlinux.h
+./bpf/libbpf/update.sh
+go generate ./...
--- build.sh
+++ build.sh
@@ -2,18 +2,5 @@
 
 set -euo pipefail
 
-export CGO_ENABLED=0
-
-/usr/sbin/bpftool btf dump file /sys/kernel/btf/vmlinux format c > bpf/common/vmlinux.h
-./bpf/libbpf/update.sh
-
-go generate ./...
-
-mkdir -p dist
-for cmd in cmd/*; do
-    cmd_name=$(basename "${cmd}")
-    if [ "${cmd_name}" != "tc_cpumap" ]; then
-        cmd_name="tc_cpumap_${cmd_name}"
-    fi
-    (cd dist && go build -o "${cmd_name}" "../${cmd}")
-done
+curl -sfL https://goreleaser.com/static/run | \
+  bash -s -- release --clean --skip=publish --snapshot
