005cc6df8cae973b1c0bf634874cf2265b0eb4a8
Brad Cowie <brad@faucet.nz>
2023-12-06 11:17:17 +1300

    feat: Set NIC coalesce configuration

--- cmd/tc_cpumap/types.go
+++ cmd/tc_cpumap/types.go
@@ -1,5 +1,16 @@
 package main
 
+import "github.com/safchain/ethtool"
+
+type EthtoolFeatures map[string]bool
+
+type EthtoolCoalesceConfig map[string]uint32
+
+type EthtoolConfig struct {
+	Features EthtoolFeatures
+	Coalesce ethtool.Coalesce
+}
+
 type XpsMask []byte
 
 type TxQueueXpsConfig map[string]XpsMask
