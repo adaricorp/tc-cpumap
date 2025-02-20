8d2be1234a5df56fc83711a2e0cd2fb6b019c0e2
Brad Cowie <brad@faucet.nz>
2025-02-20 14:21:04 +1300

    feat: display AS information for remote IP statistics

--- cmd/trafficstats/main.go
+++ cmd/trafficstats/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/netip"
 	"os"
 	"path"
+	"slices"
 	"sort"
 	"time"
 
+	"github.com/IncSW/geoip2"
 	"github.com/adaricorp/tc-cpumap/bpf"
 	"github.com/adaricorp/tc-cpumap/mac"
 	"github.com/adaricorp/tc-cpumap/tc"
@@ -27,7 +30,10 @@ var (
 	bpfMaps     = []string{"local", "remote"}
 	perCpuStats *bool
 	sortColumn  *int
+	asnDBPath   *string
 	bootTime    time.Time
+
+	asnReader *geoip2.ASNReader
 )
 
 type hostKey struct {
@@ -59,6 +65,11 @@ func init() {
 		0,
 		"Column number to sort on, or 0 to disable sorting",
 	)
+	asnDBPath = fs.StringLong(
+		"maxmind-asn-db-path",
+		"",
+		"Path to GeoLite2-ASN.mmdb file (optional)",
+	)
 
 	err := ff.Parse(fs, os.Args[1:])
 	if err != nil {
@@ -78,61 +89,47 @@ func printMap(mapName string, m *ebpf.Map) error {
 	t := table.NewWriter()
 	t.SetTitle(fmt.Sprintf("Traffic stats - %v", mapName))
 
-	if *perCpuStats {
-		t.AppendHeader(
-			table.Row{
-				"IP",
-				"MAC",
-				"RX CPU",
-				"Sent",
-				"Sent",
-				"Received",
-				"Received",
-				"TC handle",
-				"Last seen",
-			},
-			table.RowConfig{AutoMerge: true},
-		)
-		t.AppendHeader(
-			table.Row{
-				"",
-				"",
-				"",
-				"Bytes",
-				"Packets",
-				"Bytes",
-				"Packets",
-				"",
-				"",
-			},
-		)
-	} else {
-		t.AppendHeader(
-			table.Row{
-				"IP",
-				"MAC",
-				"Sent",
-				"Sent",
-				"Received",
-				"Received",
-				"TC handle",
-				"Last seen",
-			},
-			table.RowConfig{AutoMerge: true},
-		)
-		t.AppendHeader(
-			table.Row{
-				"",
-				"",
-				"Bytes",
-				"Packets",
-				"Bytes",
-				"Packets",
-				"",
-				"",
-			},
-		)
+	headingRow := table.Row{
+		"IP",
+		"MAC",
+		"Sent",
+		"Sent",
+		"Received",
+		"Received",
+		"TC handle",
+		"Last seen",
 	}
+
+	subheadingRow := table.Row{
+		"",
+		"",
+		"Bytes",
+		"Packets",
+		"Bytes",
+		"Packets",
+		"",
+		"",
+	}
+
+	if *perCpuStats {
+		headingRow = slices.Insert(headingRow, 2, "RX CPU")
+		subheadingRow = slices.Insert(subheadingRow, 2, "")
+	}
+
+	if mapName == "remote" && asnReader != nil {
+		headingRow = slices.Insert(headingRow, 1, "AS")
+		headingRow = slices.Insert(headingRow, 2, "AS")
+		headingRow = slices.Insert(headingRow, 3, "AS")
+		subheadingRow = slices.Insert(subheadingRow, 1, "Number")
+		subheadingRow = slices.Insert(subheadingRow, 2, "Organization")
+		subheadingRow = slices.Insert(subheadingRow, 3, "Prefix")
+	}
+
+	t.AppendHeader(
+		headingRow,
+		table.RowConfig{AutoMerge: true},
+	)
+	t.AppendHeader(subheadingRow)
 
 	var (
 		key  bpf.BpfIn6Addr
@@ -144,9 +141,38 @@ func printMap(mapName string, m *ebpf.Map) error {
 		ip := netip.AddrFrom16(key.In6U.U6Addr8)
 		if *perCpuStats {
 			for cpu, val := range vals {
-				t.AppendRow(
-					[]interface{}{
-						ip.Unmap(),
+				row := table.Row{
+					ip.Unmap(),
+				}
+
+				if mapName == "remote" && asnReader != nil {
+					record, err := asnReader.Lookup(net.IP(ip.Unmap().AsSlice()))
+					if err != nil {
+						row = append(row,
+							table.Row{
+								"",
+								"",
+								"",
+							}...,
+						)
+					} else {
+						prefix := record.Network
+						if prefix == "<nil>" {
+							prefix = ""
+						}
+
+						row = append(row,
+							table.Row{
+								record.AutonomousSystemNumber,
+								record.AutonomousSystemOrganization,
+								prefix,
+							}...,
+						)
+					}
+				}
+
+				row = append(row,
+					table.Row{
 						mac.MacAddress(val.Mac),
 						cpu,
 						val.TxBytes,
@@ -156,8 +182,10 @@ func printMap(mapName string, m *ebpf.Map) error {
 						tc.TcHandleString(val.TcHandle),
 						val.LastSeen,
 						decodeLastSeenTime(val.LastSeen),
-					},
+					}...,
 				)
+
+				t.AppendRow(row)
 			}
 		} else {
 			// Sort by last seen so TcHandle/LastSeen values that
@@ -197,9 +225,38 @@ func printMap(mapName string, m *ebpf.Map) error {
 			}
 
 			for host, stats := range hostAggData {
-				t.AppendRow(
-					[]interface{}{
-						ip.Unmap(),
+				row := table.Row{
+					ip.Unmap(),
+				}
+
+				if mapName == "remote" && asnReader != nil {
+					record, err := asnReader.Lookup(net.IP(ip.Unmap().AsSlice()))
+					if err != nil {
+						row = append(row,
+							table.Row{
+								"",
+								"",
+								"",
+							}...,
+						)
+					} else {
+						prefix := record.Network
+						if prefix == "<nil>" {
+							prefix = ""
+						}
+
+						row = append(row,
+							table.Row{
+								record.AutonomousSystemNumber,
+								record.AutonomousSystemOrganization,
+								prefix,
+							}...,
+						)
+					}
+				}
+
+				row = append(row,
+					table.Row{
 						mac.MacAddress(host.mac),
 						stats.TxBytes,
 						stats.TxPackets,
@@ -207,8 +264,10 @@ func printMap(mapName string, m *ebpf.Map) error {
 						stats.RxPackets,
 						tc.TcHandleString(stats.TcHandle),
 						decodeLastSeenTime(stats.LastSeen),
-					},
+					}...,
 				)
+
+				t.AppendRow(row)
 			}
 		}
 	}
@@ -237,6 +296,14 @@ func main() {
 		log.Fatalf("Error getting system boot time: %v", err)
 	}
 	bootTime = time.Now().Add(-1 * time.Duration(bootTimespec.Nano()))
+
+	if *asnDBPath != "" {
+		var err error
+		asnReader, err = geoip2.NewASNReaderFromFile(*asnDBPath)
+		if err != nil {
+			log.Fatalf("Error opening ASN database: %s", err)
+		}
+	}
 
 	for _, mapName := range bpfMaps {
 		mapFileName := fmt.Sprintf("map_traffic_%v", mapName)
